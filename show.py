f4789b2bd8704e0d3253213b26ffc636d0108355
miszTom <tomzsuu@icloud.com>
2024-05-14 14:36:37 +0900

    fix conflict

--- show.py
+++ show.py
@@ -12,13 +12,10 @@ import random
 
 def main():
 
-<<<<<<< HEAD
-
-=======
     with open("./hyakunin.txt", encoding="utf-8") as f:
         wakas = [s.strip() for s in f.readlines()]
         print("今日の一句：　" + wakas[random.randrange(len(wakas))])
 
 if __name__ == '__main__':
     main()
->>>>>>> add_main
+
