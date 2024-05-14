7714ad53b8a183aef2ed9ddad673ce70c21e8fc0
miszTom <tomzsuu@icloud.com>
2024-05-14 14:03:40 +0900

    mod dir back first version

--- show.py
+++ show.py
@@ -3,7 +3,7 @@
 
 import random
 
-with open("C:\Users\misz8\aidemy_git/hyakunin.txt", encoding="utf-8") as f:
+with open("./hyakunin.txt", encoding="utf-8") as f:
     wakas = [s.strip() for s in f.readlines()]
 
 print(wakas[random.randrange(len(wakas))])
